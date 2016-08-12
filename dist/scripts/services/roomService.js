8f16a92824fc412fb12171c0a550c4352fef6bcd
gitbnw <byron.weiss@gmail.com>
2016-08-11 21:31:13 -0500

    minor fix

--- dist/scripts/services/roomService.js
+++ dist/scripts/services/roomService.js
@@ -1,15 +1,14 @@
 (function() {
     function roomService($firebaseArray) {
-        var ref = firebase.database().ref().child("rooms");
+        var ref = firebase.database().ref("rooms");
         var rooms = $firebaseArray(ref);
 
         return {
             all: rooms,
             getMessages: function(roomId) {
                 // getMessages query logic
-               var ref = firebase.database().ref("messages")
+               var ref = firebase.database().ref("messages");
                var messages = $firebaseArray(ref.orderByChild("roomId").equalTo(roomId))
-               console.log(messages)
                return messages
             }
 
